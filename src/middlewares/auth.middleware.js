b126bc684372f741cf06291425567dc268d64c81
Aditya Sharma <adityasd7502@gmail.com>
2026-01-30 06:09:13 +0530

    - Update user controller - Write auth middleware - Add login route

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,30 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+
+const verifyJwt = asyncHandler(async (req, _, next) => {
+    try {
+        const accessToken = req.cookies?.accessToken || req.header?.("Authorization")?.replace("Bearer ", "");
+
+        if (!accessToken) {
+            throw new ApiError(401, "Unauthorized");
+        }
+
+        const decodedToken = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+
+        const user = await User.findById(decodedToken?._id)
+            .select("-password -createdAt -updatedAt -refreshToken");
+
+        if (!user) {
+            throw new ApiError(401, "Unauthorized");
+        }
+
+        req.user = user;
+        next();
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Unauthorized");
+    }
+});
+
+export { verifyJwt };
