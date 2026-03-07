045083f69bf4d734bf8a311e4ef3737f089addd5
Aditya Sharma <adityasd7502@gmail.com>
2026-03-07 15:54:26 +0530

    Added new routes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,16 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import {
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+    registerUser,
+    getCurrentUser,
+    updateAccount,
+    updateAvatar,
+    updateCoverImage,
+    getWatchHistory,
+    getUserChannelProfile
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 
@@ -28,5 +39,26 @@ router.route("/logout")
 
 router.route("/refresh-access-token")
     .post(refreshAccessToken);
+
+router.route("/change-password")
+    .post(verifyJwt, changeCurrentPassword);
+
+router.route("/current-user")
+    .get(verifyJwt, getCurrentUser);
+
+router.route("/update-account")
+    .patch(verifyJwt, updateAccount);
+
+router.route("/update-avatar")
+    .patch(verifyJwt, upload.single("avatar"), updateAvatar);
+
+router.route("/update-cover-image")
+    .patch(verifyJwt, upload.single("coverImage"), updateCoverImage);
+
+router.route("/c/:username")
+    .get(verifyJwt, getUserChannelProfile);
+
+router.route("/watch-history")
+    .get(verifyJwt, getWatchHistory);
 
 export { router };
